f08575c9cac18da5260977d3c462cd47c2048cfa
hestabshim <amberzeng@gmail.com>
2025-01-28 22:50:45 -0500

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -3,13 +3,19 @@ using namespace std;
 int main()
 {
 	std::cout << "My name is Amber. Help me solve this equation for x. Ax + B = 0" << std::endl;
+	
 	float A;
 	float B;
 	float X;
+	
 	cout << "Please enter a value for A: ";
 	cin >> A;
 	cout << "A=" << A << endl;
-	B = (A * 4) + 1;
+	cout << "Please enter a value for B: ";
+	cin >> B;
 	cout << "And B=" << B << endl;
+	
+	X = (-B / A);
+	
+	cout << "X=" << X << endl;
 }
-
